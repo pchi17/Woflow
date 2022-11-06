151327b962e5a13200e3f76f7aceccb9a90fdcb2
Philip <pengfei.p.chi@gmail.com>
2022-11-06 14:52:49 -0800

    Update Solution1.java
    Batching HTTP calls

--- Solution1.java
+++ Solution1.java
@@ -14,16 +14,15 @@ public class Solution1 {
     private static final String path = "https://nodes-on-nodes-challenge.herokuapp.com/nodes/";
     private final Map<String, Set<String>> graph = new HashMap<>();
     private final Queue<String> queue = new LinkedList<>();
-
     private String getNodePath(String nodeID) {
         return path + nodeID;
     }
 
+    // making HTTP get call, possibly with multiply node ids.
     private JSONArray getResponse(String nodeID) throws IOException {
         URL url = new URL(getNodePath(nodeID));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        int responseCode = con.getResponseCode();
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -35,22 +34,30 @@ public class Solution1 {
         return new JSONArray(response.toString());
     }
 
-    private JSONArray getChildren(String nodeId) throws IOException {
-        JSONObject obj = getResponse(nodeId).getJSONObject(0);
-        return obj.getJSONArray("child_node_ids");
-    }
-
     private void doBFS(String headNodeID) throws IOException {
         queue.add(headNodeID);
+
         while (!queue.isEmpty()) {
-            String id = queue.poll();
-            if (graph.containsKey(id)) continue;
-            graph.put(id, new HashSet<>());
-            JSONArray children = getChildren(id);
-            for (int i = 0; i < children.length(); i++) {
-                String child = (String) children.get(i);
-                graph.get(id).add(child);
-                queue.add(child);
+            List<String> allIDs = new ArrayList<>();
+
+            while (!queue.isEmpty()) {
+                String id = queue.poll();
+                if (graph.containsKey(id)) continue;
+                allIDs.add(id);
+                graph.put(id, new HashSet<>());
+            }
+            // doing a batch call with multiple node IDs. saves the number of HTTP calls
+            JSONArray response = getResponse(String.join(",", allIDs));
+
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject jsonObj = response.getJSONObject(i);
+                String id = jsonObj.getString("id");
+                JSONArray children = jsonObj.getJSONArray("child_node_ids");
+                for (int j = 0; j < children.length(); j++) {
+                    String child = children.getString(j);
+                    graph.get(id).add(child);
+                    queue.add(child);
+                }
             }
         }
 
@@ -78,7 +85,7 @@ public class Solution1 {
         String head = "089ef556-dfff-4ff2-9733-654645be56fe";
         Solution1 solution = new Solution1();
         solution.doBFS(head);
-        
+
         // total unique nodes = 30
         // The most common node is a06c90bf-e635-4812-992e-f7b1e2408a3f
         // The frequency of it is 3
